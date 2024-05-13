c3a523d0d3e0f4b16956e661df675d307f1ce8d8
liwei3013 <liwei455@huawei.com>
2024-05-13 21:28:06 +0800

    Match-id-3773ce0e778f32d0a9ae78c4e1a6741532bef3e4

--- testcode/script/build_hitls.sh
+++ testcode/script/build_hitls.sh
@@ -61,6 +61,7 @@ build_hitls_code()
 
     # Compile openHiTLS
     cd ${HITLS_ROOT_DIR}/build
+    add_options="${add_options} -DHITLS_EAL_INIT_OPTS=1 -DHITLS_CRYPTO_ASM_CHECK" # Get CPU capability
     python3 ../configure.py --enable ${bsl_features} hitls_crypto hitls_tls hitls_x509 --bits=$BITS --system=linux
     if [[ $get_arch = "x86_64" ]]; then
         echo "Compile: env=x86_64, c, little endian, 64bits"
