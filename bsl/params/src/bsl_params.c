b761920381b81e8e428e441cd705811f1dcc9dd5
liwei3013 <liwei455@huawei.com>
2024-11-22 21:16:44 +0800

    remove some unnecessary unload

--- bsl/params/src/bsl_params.c
+++ bsl/params/src/bsl_params.c
@@ -32,6 +32,7 @@ int32_t BSL_PARAM_InitValue(BSL_Param *param, int32_t key, uint32_t type, void *
         case BSL_PARAM_TYPE_UINT32_PTR:
         case BSL_PARAM_TYPE_FUNC_PTR:
         case BSL_PARAM_TYPE_CTX_PTR:
+        case BSL_PARAM_TYPE_INT32:
             param->value = val;
             param->valueLen = valueLen;
             param->valueType = type;
@@ -149,6 +150,7 @@ int32_t BSL_PARAM_GetValue(const BSL_Param *param, int32_t key, uint32_t type, v
         case BSL_PARAM_TYPE_UINT32:
         case BSL_PARAM_TYPE_OCTETS:
         case BSL_PARAM_TYPE_BOOL:
+        case BSL_PARAM_TYPE_INT32:
             if (*valueLen < param->valueLen) {
                 BSL_ERR_PUSH_ERROR(BSL_INVALID_ARG);
                 return BSL_INVALID_ARG;
