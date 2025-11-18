08d8f891f5a3e5c880ebf64510be390df0fdf2c0
gsp114514 <gaosiping1@huawei.com>
2025-11-18 22:13:52 +0800

    fix:CRL revocation support configuration
    Co-authored-by: gsp114514<gaosiping1@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !821 merge tcrlAdd into openhitls-0.2
    
    fix:CRL revocation support configuration
    
    Created-by: gsp114514
    Commit-by: gsp114514
    Merged-by: liwei3013
    Description: fix:CRL revocation support configuration
    
    See merge request: openHiTLS/openhitls!821

--- testcode/sdv/testcase/tls/interface_tlcp/test_suite_sdv_frame_cert_interface.c
+++ testcode/sdv/testcase/tls/interface_tlcp/test_suite_sdv_frame_cert_interface.c
@@ -40,6 +40,7 @@
 #include "hitls_crypt_reg.h"
 #include "hitls_crypt_init.h"
 #include "uio_base.h"
+#include "hitls_pki_types.h"
 /* END_HEADER */
 
 #define BUF_MAX_SIZE 4096
@@ -1086,11 +1087,14 @@ void UT_TLS_CRL_VERIFICATION_HANDSHAKE_TC001(void)
     ASSERT_TRUE(server != NULL);
 
     ASSERT_EQ(HITLS_CFG_LoadCrlFile(config, crlPath, TLS_PARSE_FORMAT_ASN1), HITLS_SUCCESS);
-
     client = FRAME_CreateLinkBase(config, BSL_UIO_TCP, false);
     ASSERT_TRUE(client != NULL);
+    HITLS_CFG_SetVerifyFlags(config, HITLS_X509_VFY_FLAG_CRL_DEV);
 
     ASSERT_NE(FRAME_CreateConnection(client, server, true, HS_STATE_BUTT), HITLS_SUCCESS);
+    HITLS_ERROR ret;
+    HITLS_GetVerifyResult(client->ssl, &ret);
+    ASSERT_EQ(ret, HITLS_X509_ERR_VFY_CERT_REVOKED);
 EXIT:
     HITLS_CFG_FreeConfig(config);
     FRAME_FreeLink(client);
