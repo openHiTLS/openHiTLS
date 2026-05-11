2c2deeb478252c6a60677a14e4a5cb8fca860189
Dongjianwei001 <dongjianwei1@huawei.com>
2026-05-11 20:04:36 +0800

    Fix code review issues
    Cherry-picked from: https://gitcode.com/openHiTLS/openhitls/merge_requests/1374

--- crypto/modes/src/modes_cfb.c
+++ crypto/modes/src/modes_cfb.c
@@ -17,6 +17,7 @@
 #ifdef HITLS_CRYPTO_CFB
 
 #include <stdbool.h>
+#include <stdint.h>
 #include <string.h>
 #include "bsl_err_internal.h"
 #include "bsl_sal.h"
@@ -331,6 +332,10 @@ int32_t MODES_CFB_Encrypt(MODES_CipherCFBCtx *ctx, const uint8_t *in, uint8_t *o
     switch (ctx->feedbackBits) {
 #ifndef HITLS_CRYPTO_CFB128
         case 1:
+            if (len > UINT32_MAX / 8) {
+                BSL_ERR_PUSH_ERROR(CRYPT_MODE_ERR_INPUT_LEN);
+                return CRYPT_MODE_ERR_INPUT_LEN;
+            }
             return MODES_CFB_BitCrypt(ctx, in, out, len * 8, true); // Each byte occupies 8 bits.
         case 8:
         case 64:
@@ -353,6 +358,10 @@ int32_t MODES_CFB_Decrypt(MODES_CipherCFBCtx *ctx, const uint8_t *in, uint8_t *o
     switch (ctx->feedbackBits) {
 #ifndef HITLS_CRYPTO_CFB128
         case 1:     // 1-bit cfb. Convert the length of bytes to the length of bits.
+            if (len > UINT32_MAX / 8) {
+                BSL_ERR_PUSH_ERROR(CRYPT_MODE_ERR_INPUT_LEN);
+                return CRYPT_MODE_ERR_INPUT_LEN;
+            }
             return MODES_CFB_BitCrypt(ctx, in, out, len * 8, false); // Each byte occupies 8 bits.
         case 8:     // 8-bit cfb
         case 64:    // 64-bit cfb
